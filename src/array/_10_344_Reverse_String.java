8c29a2c16258fbb803558af6c22168ba65609979
杨育才 <1127424957@qq.com>
2019-03-18 23:15:20 +0800

    更改交换方式

--- src/array/_10_344_Reverse_String.java
+++ src/array/_10_344_Reverse_String.java
@@ -10,14 +10,16 @@ public class _10_344_Reverse_String {
         int i = 0;
         int j = s.length - 1;
         while(i < j) {
-            s[i] = (char)(s[i] ^ s[j]);
-            s[j] = (char)(s[i] ^ s[j]);
-            s[i] = (char)(s[i] ^ s[j]);
-            i++;
-            j--;
+            swap(s, i++, j--);
         }
     }
-    
+
+    private void swap(char[] chars, int i, int j) {
+        char t = chars[i];
+        chars[i] = chars[j];
+        chars[j] = t;
+    }
+
     public static void main(String[] args) {  
         String str = "hello";
         char[] chars = str.toCharArray();
